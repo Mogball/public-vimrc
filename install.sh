7966d007de9acc3e8523027cd0e62cc9077efef9
Jeff Niu <jeffniu22@gmail.com>
2020-07-09 21:44:43 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,3 @@ cp .tmux.conf ~/.tmux.conf
 cp .vimrc ~/.vimrc
 mkdir -p ~/.config/nvim
 cp init.vim ~/.config/nvim/init.vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
